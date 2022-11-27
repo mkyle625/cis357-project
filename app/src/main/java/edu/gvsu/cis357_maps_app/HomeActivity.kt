c3e2ed36ea9677d5d183a20a216d72d289a37aee
Kyle <40324912+mkyle625@users.noreply.github.com>
2022-11-27 11:55:56 -0500

    Some changes, added maps sdk

---
+++ app/src/main/java/edu/gvsu/cis357_maps_app/HomeActivity.kt
@@ -0,0 +1,11 @@
+package edu.gvsu.cis357_maps_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
